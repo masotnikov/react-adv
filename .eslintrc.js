58f208574e6b68cdb711d24a3bbe3d36159b66e1
masotnikov <masotnikov11@yandex.ru>
2024-11-17 17:56:10 +0300

    add stylelint, eslint

---
+++ .eslintrc.js
@@ -0,0 +1,52 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'plugin:i18next/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+    'i18next',
+  ],
+  rules: {
+    'linebreak-style': 0,
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
+    'import/no-unresolved': 'off',
+    'import/prefer-default-export': 'off',
+    'no-unused-vars': 'warn',
+    'react/require-default-props': 'off',
+    'react/jsx-props-no-spreading': 'warn',
+    'react/function-component-definition': 'off',
+    'no-shadow': 'off',
+    'import/extensions': 'off',
+    'import/no-extraneous-dependencies': [
+      'warn',
+      {
+        devDependencies: [
+          '**/webpack.config.ts',
+          '**/config/build/*',
+        ],
+      },
+    ],
+    'no-underscore-dangle': 'off',
+    'implicit-arrow-linebreak': 'warn',
+    'i18next/no-literal-string': ['error', { markupOnly: true }],
+  },
+  globals: {
+    __IS_DEV__: true,
+  },
+};
