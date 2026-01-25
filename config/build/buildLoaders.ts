9de98fdc97e0de51982ea33d02987bea113b143a
masotnikov <masotnikov11@yandex.ru>
2026-01-26 00:44:41 +0300

    refactoring loaders

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,14 +1,40 @@
 import webpack from "webpack";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import {BuildOptions} from "./types/config";
 
-export function buildLoaders(): webpack.RuleSetRule[] {
+export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
+  // нужен babel если не используется typescript
+  const typescriptLoader = {
+    test: /\.tsx?$/,
+    use: 'ts-loader',
+    exclude: /node_modules/,
+  }
 
-    const typescriptLoader = {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-    }
+  const cssLoader = {
+    test: /\.s[ac]ss$/i,
+    use: [
+      // Creates `style` nodes from JS strings
+      options.isDev ? "style-loader" : MiniCssExtractPlugin.loader,
+      // Translates CSS into CommonJS
+      {
+        loader: "css-loader",
+        options: {
+          modules: {
+            auto: ((resourcePath: string) => resourcePath.includes('.module.')),
+            localIdentName: options.isDev ?
+              "[path][name]__[local]--[hash:base64:5]"
+              :
+              "[hash:base64]",
+          },
+        }
+      },
+      // Compiles Sass to CSS
+      "sass-loader",
+    ],
+  }
 
-    return [
-        typescriptLoader,
-    ]
+  return [
+    typescriptLoader,
+    cssLoader,
+  ]
 }
