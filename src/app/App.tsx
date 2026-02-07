52de91226721c51647b80ccfdcc2842e90c800de
masotnikov <masotnikov11@yandex.ru>
2026-02-07 23:26:14 +0300

    moved files

---
+++ src/app/App.tsx
@@ -0,0 +1,21 @@
+import "./styles/index.scss"
+import {Link} from "react-router-dom";
+import {classNames} from "@/shared/lib/classNames/classNames";
+import {useTheme} from "@/app/providers/ThemePovider";
+import {AppRouter} from "@/app/providers/router";
+
+
+const App = () => {
+  const {theme, toggleTheme} = useTheme();
+
+  return (
+    <div className={classNames("app", {}, [theme])}>
+      <button onClick={toggleTheme}>Toggle Theme</button>
+      <Link to={"/"}>Главная</Link>
+      <Link to={"/about"}>О сайте</Link>
+      <AppRouter/>
+    </div>
+  );
+};
+
+export default App;
--- src/app/providers/ThemePovider/ui/ThemeProvider.tsx
+++ src/app/providers/ThemePovider/ui/ThemeProvider.tsx
@@ -4,7 +4,7 @@ import {LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext} from "./ThemeContext";
 const defaultTheme = localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme || Theme.LIGHT;
 
 
-const ThemeProvider: FC = ({children}) => {
+export const ThemeProvider: FC = ({children}) => {
   const [theme, setTheme] = useState<Theme>(defaultTheme);
 
   const contextValue = useMemo(() => ({
@@ -18,5 +18,3 @@ const ThemeProvider: FC = ({children}) => {
     </ThemeContext.Provider>
   );
 };
-
-export default ThemeProvider;
