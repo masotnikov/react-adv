9f7b8fd9f2dcc2ffcfff9eb47decadbf85be9e54
masotnikov <masotnikov11@yandex.ru>
2026-02-12 23:58:46 +0300

    add Navbar

---
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -0,0 +1,22 @@
+import {FC} from "react";
+import {classNames} from "@/shared/lib/classNames/classNames";
+import cls from "./Navbar.module.scss";
+import {AppLink, AppLinkTheme} from "@/shared/ui/AppLink/AppLink";
+
+interface NavbarProps {
+  className?: string;
+}
+
+export const Navbar: FC<NavbarProps> = (props) => {
+  const {className} = props;
+
+  return (
+    <div className={classNames(cls.Navbar, {}, [className])}>
+      <div className={cls.links}>
+        <AppLink theme={AppLinkTheme.SECONDARY} className={cls.mainLink} to={"/"}>Главная</AppLink>
+        <AppLink theme={AppLinkTheme.SECONDARY} to={"/about"}>О сайте</AppLink>
+      </div>
+    </div>
+  );
+};
+
