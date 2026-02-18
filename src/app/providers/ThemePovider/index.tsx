894e49742e6108c2ecc1020909d3b109295e92cd
masotnikov <masotnikov11@yandex.ru>
2026-02-19 00:36:48 +0300

    refactor export

--- src/app/providers/ThemePovider/index.tsx
+++ src/app/providers/ThemePovider/index.tsx
@@ -1,7 +1,9 @@
 import {ThemeProvider} from "./ui/ThemeProvider"
 import {useTheme} from "./lib/useTheme"
+import {Theme} from "./ui/ThemeContext"
 
 export {
   useTheme,
-  ThemeProvider
+  ThemeProvider,
+  Theme
 }
