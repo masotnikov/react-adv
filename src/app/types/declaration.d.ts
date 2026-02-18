0916cd7e4969f4134346813305f8424ce3355c41
masotnikov <masotnikov11@yandex.ru>
2026-02-19 00:30:54 +0300

    add declarations

--- src/app/types/declaration.d.ts
+++ src/app/types/declaration.d.ts
@@ -2,3 +2,13 @@ declare module '*.scss' {
   const content: Record<string, string>;
   export default content;
 }
+
+declare module "*.png";
+declare module "*.jpeg";
+declare module "*.jpg";
+
+declare module "*.svg" {
+  import {FC, SVGProps} from "react";
+  const content: FC<SVGProps<SVGElement>>;
+  export default content;
+}
