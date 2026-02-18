b6208a5358f0b09564dba33da64f123b25971b7e
masotnikov <masotnikov11@yandex.ru>
2026-02-19 00:31:16 +0300

    add sidebar

---
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -0,0 +1,24 @@
+import {FC, useState} from "react";
+import {classNames} from "@/shared/lib/classNames/classNames";
+import cls from "./Sidebar.module.scss";
+import {ThemeSwitcher} from "@/shared/ui/ThemeSwitcher";
+
+interface SidebarProps {
+  className?: string;
+}
+
+export const Sidebar: FC<SidebarProps> = (props) => {
+  const {className} = props;
+  const [collapsed, setCollapsed] = useState(false);
+  const onToggle = () => setCollapsed(prevState => !prevState)
+
+  return (
+    <div className={classNames(cls.Sidebar, {[cls.collapsed]: collapsed}, [className])}>
+      <button onClick={onToggle}>toggle</button>
+      <div className={cls.switchers}>
+        <ThemeSwitcher/>
+      </div>
+    </div>
+  );
+};
+
