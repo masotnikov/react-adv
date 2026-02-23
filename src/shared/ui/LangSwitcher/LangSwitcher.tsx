a2e4c99b943d64fef31d0fc80bed31ccb5a67a16
masotnikov <masotnikov11@yandex.ru>
2026-02-23 23:41:43 +0300

    add langswitcher

---
+++ src/shared/ui/LangSwitcher/LangSwitcher.tsx
@@ -0,0 +1,28 @@
+import {FC} from "react";
+import {useTranslation} from "react-i18next";
+import {Button, ThemeButton} from "@/shared/ui/Button/Button";
+import {classNames} from "@/shared/lib/classNames/classNames";
+import cls from "@/shared/ui/LangSwitcher/LangSwitcher.module.scss";
+
+interface LangSwitcherProps {
+  className?: string;
+}
+
+export const LangSwitcher: FC<LangSwitcherProps> = (props) => {
+  const {t, i18n} = useTranslation();
+  const {className} = props;
+
+  const toggleLanguage = () => {
+    i18n.changeLanguage(i18n.language === "ru" ? "en" : "ru");
+  }
+
+  return (
+    <Button
+      className={classNames(cls.LangSwitcher, {}, [className])}
+      theme={ThemeButton.CLEAR}
+      onClick={toggleLanguage}
+    >
+      {t("Язык")}
+    </Button>
+  )
+};
