0c95c35edb55f8c1e8819285400d302f67549efe
masotnikov <masotnikov11@yandex.ru>
2026-02-23 23:43:27 +0300

    add translate

--- src/pages/AboutPage/ui/AboutPage.tsx
+++ src/pages/AboutPage/ui/AboutPage.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import {useTranslation} from "react-i18next";
 
 const AboutPage = () => {
+
+  const {t} = useTranslation("about");
+
   return (
     <div>
-      <h1>AboutPage</h1>
+      <h1>{t("О сайте")}</h1>
     </div>
   );
 };
